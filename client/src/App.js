71be248e3d5e7d382f8821c358ca87a1cc60024f
DK Kim <dkim693@wisc.edu>
2023-11-04 17:37:54 -0500

    running now

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
  // We import all the components we need in our app
 import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
 
  const App = () => {
  return (
    <div>
-
+    <Navbar>
      <Routes>
-
+       <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/create" element={<Create />} />
      </Routes>
+     </Navbar>
    </div>
  );
 };
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,4 @@
 import React from "react";
-// We import bootstrap to make our application look better.
-import "bootstrap/dist/css/bootstrap.css";
  // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
  // Here, we display our Navbar
