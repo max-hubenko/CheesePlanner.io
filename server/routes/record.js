56221e6f9410d719b1c5f46e4c9a538ca2c5a918
DK Kim <dkim693@wisc.edu>
2023-11-04 22:08:06 -0500

    same

--- server/routes/record.js
+++ server/routes/record.js
@@ -77,7 +77,7 @@ recordRoutes.route("/update/:id").post(function (req, response) {
 // This section will help you delete a record
 recordRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
+  let myquery = { _id: new ObjectId(req.params.id) };
   db_connect.collection("records").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
